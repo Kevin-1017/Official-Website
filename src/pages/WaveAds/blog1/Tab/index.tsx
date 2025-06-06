07158522f40d47eafb2d012be1824bc6ea2e7b36
kevin.huang <kevin.huang@tec-do.com>
2025-06-06 15:38:33 +0800

    feat[+]:waveads迭代1

--- src/pages/WaveAds/blog1/Tab/index.tsx
+++ src/pages/WaveAds/blog1/Tab/index.tsx
@@ -1,8 +1,9 @@
 import styles from "./index.module.scss";
 import logo from "../Public/logo.jpg";
 import language from "../Public/language.png";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+import domainList from "@/utils/data/domainList";
 const TabList = () => {
   const [showList, setShowList] = useState(false);
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -49,50 +50,55 @@ const TabList = () => {
   };
 
   const nagative = useNavigate();
+  const currentHostName = useMemo(() => window.location.hostname, []);
   const toContact = () => {
-    document
-      .getElementById("contact-us")
-      ?.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
-  return (
-    <div
-      className={styles.tab}
-      style={{
-        transform: isVisible ? "translateY(0)" : "translateY(-100%)",
-        opacity: isVisible ? 1 : 0,
-      }}
-    >
-      <div className={styles.left}>
-        <img
-          src={logo}
-          alt=""
-          onClick={() => {
-            nagative("/waveads1/home");
-            window.location.reload();
-          }}
-        />
-        <ul>
-          <li></li>
-        </ul>
-      </div>
-      <div className={styles.right} ref={wrapperRef}>
-        <img src={language} alt="" onClick={() => setShowList(!showList)} />
-        {/* 英语、葡萄牙、西班牙、韩语、日语 */}
-        {showList && (
+    if (currentHostName === domainList.waveads[1]) {
+      nagative("/contact-us");
+    } else {
+      nagative("/waveads1/contact-us");
+    }
+    return (
+      <div
+        className={styles.tab}
+        style={{
+          transform: isVisible ? "translateY(0)" : "translateY(-100%)",
+          opacity: isVisible ? 1 : 0,
+        }}
+      >
+        <div className={styles.left}>
+          <img
+            src={logo}
+            alt=""
+            onClick={() => {
+              nagative("/waveads1/home");
+              window.location.reload();
+            }}
+          />
           <ul>
-            <li onClick={() => changeLang("en")}>{window.$t("English")}</li>
-            <li onClick={() => changeLang("pt")}>{window.$t("Portuguese")}</li>
-            <li onClick={() => changeLang("es")}>{window.$t("Spanish")}</li>
-            <li onClick={() => changeLang("ko")}>{window.$t("Korean")}</li>
-            <li onClick={() => changeLang("ja")}>{window.$t("Japanese")}</li>
+            <li></li>
           </ul>
-        )}
-        <div className={styles.contact} onClick={toContact}>
-          {window.$t("CONTACT US")}
+        </div>
+        <div className={styles.right} ref={wrapperRef}>
+          <img src={language} alt="" onClick={() => setShowList(!showList)} />
+          {/* 英语、葡萄牙、西班牙、韩语、日语 */}
+          {showList && (
+            <ul>
+              <li onClick={() => changeLang("en")}>{window.$t("English")}</li>
+              <li onClick={() => changeLang("pt")}>
+                {window.$t("Portuguese")}
+              </li>
+              <li onClick={() => changeLang("es")}>{window.$t("Spanish")}</li>
+              <li onClick={() => changeLang("ko")}>{window.$t("Korean")}</li>
+              <li onClick={() => changeLang("ja")}>{window.$t("Japanese")}</li>
+            </ul>
+          )}
+          <div className={styles.contact} onClick={toContact}>
+            {window.$t("CONTACT US")}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default TabList;
