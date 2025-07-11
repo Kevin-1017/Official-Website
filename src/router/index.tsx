d06c221db5c4c0a642f204855b809b73ee49faae
kevin.huang <kevin.huang@tec-do.com>
2025-07-11 11:07:51 +0800

    feat[+]]:注释不需要的官网路由配置项

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,8 +3,8 @@ import { useMemo } from "react";
 import domainList from "@/utils/data/domainList";
 import kuponz1Routes from "./modules/kuponz1";
 import kuponz2Routes from "./modules/kuponz2";
-import waveads1Routes from "./modules/waveads1";
-import privacyRoutes from "./modules/privacypolicy";
+// import waveads1Routes from "./modules/waveads1";
+// import privacyRoutes from "./modules/privacypolicy";
 
 //路由命名规范
 // 1. 路由名称全部使用小写字母，多个单词使用下划线连接
@@ -22,11 +22,11 @@ const Router: React.FC = () => {
     //kuponz官网二路由
     ...kuponz2Routes.test,
 
-    //waveads路由
-    ...waveads1Routes.test,
+    // //waveads路由
+    // ...waveads1Routes.test,
 
-    //privacy路由
-    ...privacyRoutes.test,
+    // //privacy路由
+    // ...privacyRoutes.test,
   ];
 
   switch (currentHostName) {
@@ -44,14 +44,14 @@ const Router: React.FC = () => {
     case domainList.kuponz[1]:
       actualRouter = [...kuponz2Routes.onLine];
       break;
-    //waveads官网
-    case domainList.waveads[0]:
-      actualRouter = [...waveads1Routes.onLine];
-      break;
-    //privacy
-    case domainList.privacy[0]:
-      actualRouter = [...privacyRoutes.onLine];
-      break;
+    // //waveads官网
+    // case domainList.waveads[0]:
+    //   actualRouter = [...waveads1Routes.onLine];
+    //   break;
+    // //privacy
+    // case domainList.privacy[0]:
+    //   actualRouter = [...privacyRoutes.onLine];
+    //   break;
   }
   const router = createBrowserRouter(actualRouter);
   return <RouterProvider router={router} />;
--- src/utils/data/domainList.ts
+++ src/utils/data/domainList.ts
@@ -1,5 +1,5 @@
 export default {
   kuponz: ["kuponz.oline.mobi", "casback.tech"],
-  waveads: ["waveads-global.com"],
-  privacy: [""],
+  // waveads: ["waveads-global.com"],
+  // privacy: [""],
 };
